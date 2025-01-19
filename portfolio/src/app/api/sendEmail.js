9c29a8cf97f6057494f6fc6aaf490035723ed2c4
Muhammad Ahmed <edgemaster@Adnans-MacBook-Air.local>
2025-01-18 21:38:20 -0500

    fixed api variables

--- portfolio/src/app/api/sendEmail.js
+++ portfolio/src/app/api/sendEmail.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend("re_3Z4TU2XR_LNCJcJtVGoRsoGmxhDoyy2KF");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
